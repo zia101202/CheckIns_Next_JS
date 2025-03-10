9eb274d947dff692c52df138331b5dadbce03ccb
ziaurrehman <ziaurrehmankhan9999@gmail.com>
2025-03-10 11:17:24 +0500

    version2

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ["res.cloudinary.com"], // Allow Cloudinary images
+      },
+      images: {
+        unoptimized: true, // Disable Next.js image optimization
+      },
+};
 
 export default nextConfig;
